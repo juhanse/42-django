81cf81ca221e021bb71a9a01b34782f1606f8dd1
juhanse <julien.hanse12@gmail.com>
2026-09-08 22:11:17 +0200

    add: starting ex02

---
+++ 0_Starting/ex02/var_to_dict.py
@@ -0,0 +1,36 @@
+def create_dict():
+	d = [
+		('Hendrix' , '1942'),
+		('Allman' , '1946'),
+		('King' , '1925'),
+		('Clapton' , '1945'),
+		('Johnson' , '1911'),
+		('Berry' , '1926'),
+		('Vaughan' , '1954'),
+		('Cooder' , '1947'),
+		('Page' , '1944'),
+		('Richards' , '1943'),
+		('Hammett' , '1962'),
+		('Cobain' , '1967'),
+		('Garcia' , '1942'),
+		('Beck' , '1944'),
+		('Santana' , '1947'),
+		('Ramone' , '1948'),
+		('White' , '1975'),
+		('Frusciante', '1970'),
+		('Thompson' , '1949'),
+	]
+
+	musicians = {}
+
+	for name, year in d:
+		if year in musicians:
+			musicians[year] += " " + name
+		else:
+			musicians[year] = name
+
+	for year, names in musicians.items():
+		print(f"{year} : {names}")
+
+if __name__ == '__main__':
+    create_dict()
